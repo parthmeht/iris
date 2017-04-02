5b050764b9f29977ccf090f18e48a67f4034ffbb
Parth Mehta <Parth Mehta>
2017-04-02 20:56:42 +0530

    Iris bot chapter 2 done..able to detect nlp commands

---
+++ Exercise Files/02_07/iris/server/slackClient.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
+let nlp = null;
+
+function handleOnAuthenticated(rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function handleOnMessage(message) {
+    
+    nlp.ask(message.text, (err, res) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+
+        if(!res.intent) {
+            return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+
+        } else if(res.intent[0].value == 'time' && res.location) {
+            return rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`, message.channel);
+        } else {
+            console.log(res);
+            return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+        }
+
+
+        rtm.sendMessage('Sorry, I did not understand.', message.channel, function messageSent() {
+            // optionally, you can supply a callback to execute once the message has been sent
+        });
+    });
+    
+
+}
+
+function addAuthenticatedHandler(rtm, handler) {
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
+
+
+module.exports.init = function slackClient(token, logLevel, nlpClient) {
+    rtm = new RtmClient(token, {logLevel: logLevel});
+    nlp = nlpClient;
+    addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
+    return rtm;
+}
+
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
---
+++ bin/run.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const slackClient = require('../server/slackClient');
+const service = require('../server/service');
+const http = require('http');
+const server = http.createServer(service);
+
+const witToken = 'GNK3BJSAZ6HQISI4JO7O7AP7PSDHRELW';
+const witClient = require('../server/witClient')(witToken);
+
+const slackToken = 'xoxb-161959011446-T9oQ0HHh5Yk2QtWjtFiXxg9J';
+const slackLogLevel = 'verbose';
+
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
+rtm.start();
+
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
+
+server.on('listening', function() {
+    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
+});
