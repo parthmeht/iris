40921b5755a14b6dd50defc789d74c6ebd96bea0
Parth Mehta <Parth Mehta>
2017-04-02 22:24:48 +0530

    New Token

--- bin/run.js
+++ bin/run.js
@@ -8,10 +8,11 @@ const server = http.createServer(service);
 const witToken = 'GNK3BJSAZ6HQISI4JO7O7AP7PSDHRELW';
 const witClient = require('../server/witClient')(witToken);
 
-const slackToken = 'xoxb-161959011446-phCt0goF5CrCAVS8Ml15Innb';
+const slackTokenPart1 = 'xoxb-161959011446-';
+const slackTokenPart2 = 'mzPWkTOxarnHzXJvKQLU7hy1';
 const slackLogLevel = 'verbose';
 
-const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
+const rtm = slackClient.init(slackTokenPart1+""+slackTokenPart2, slackLogLevel, witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
